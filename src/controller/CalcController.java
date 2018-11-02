88755c9c1a0594ea1811eedfc4e4b95daa5bd528
PAYJAY <payjay6597@gmail.com>
2018-11-02 13:52:20 +0900

    leejongwon first commit

--- src/controller/CalcController.java
+++ src/controller/CalcController.java
@@ -24,16 +24,16 @@ public class CalcController extends HttpServlet {
 		
 		switch (operator) {
 		case "+":
-			// TODO: result = Integer.parseInt(oper1) + Integer.parseInt(oper2);
+			result = Integer.parseInt(oper1) + Integer.parseInt(oper2);
 			break;
 		case "-":
-			// TODO: result = Integer.parseInt(oper1) - Integer.parseInt(oper2);
+			result = Integer.parseInt(oper1) - Integer.parseInt(oper2);
 			break;
 		case "*":
-			// TODO: result = Integer.parseInt(oper1) * Integer.parseInt(oper2);
+			result = Integer.parseInt(oper1) * Integer.parseInt(oper2);
 			break;
 		case "/":
-			// TODO: result = Integer.parseInt(oper1) / Integer.parseInt(oper2);
+			result = Integer.parseInt(oper1) / Integer.parseInt(oper2);
 			break;
 		}		
 		request.setAttribute("result", result);
